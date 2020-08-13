6042891172381acf8cb0c1aed9831af3ed3b8b77
sina <pooya.sina@gmail.com>
2020-08-13 22:40:47 +0430

    add cross origin annotation

--- src/main/java/com/sina/todoappbackend/service/MainController.java
+++ src/main/java/com/sina/todoappbackend/service/MainController.java
@@ -1,5 +1,6 @@
 package com.sina.todoappbackend.service;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import com.sina.todoappbackend.entity.Message;
 
 @RestController
 @RequestMapping(value = "todo/v1.0.1")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MainController {
 	@GetMapping(value = "sayHello")
 	public Message sayHello() {
