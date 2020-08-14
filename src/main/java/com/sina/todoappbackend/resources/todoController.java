65c83fe5eda0bb7ee0fa781fe569f5136bdf41c1
sina <pooya.sina@gmail.com>
2020-08-14 15:34:12 +0430

    add cross origin for request from localhost

--- src/main/java/com/sina/todoappbackend/resources/todoController.java
+++ src/main/java/com/sina/todoappbackend/resources/todoController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.sina.todoappbackend.entity.Todo;
 import com.sina.todoappbackend.service.HardCodedToDoService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class todoController {
 	@Autowired
 	private HardCodedToDoService toDoService;
